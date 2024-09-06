5298648c145fe482a7c62610f5c8d1900333e868
Kyunghwan Abel Bae <kyunghwan.abel.bae@gmail.com>
2024-09-06 14:14:39 +0900

    data & utill partially done

--- final_data.py
+++ final_data.py
@@ -7,44 +7,25 @@ import pandas as pd
 # from util import *
 from final_util import *
 
-from util import *
 
-
-class ContrailAshDataset_CMP1(torch.utils.data.Dataset):
+class ContrailAshDataset(torch.utils.data.Dataset):
     def __init__(self, str_folder):
-        print("init")
         self.str_data_type = str_folder
         self.df_idx = pd.DataFrame({'idx':os.listdir(os.path.join("data", str_folder))})
 
     def __getitem__(self, index):
-        img, mask = get_data(self.str_data_type, self.df_idx.iloc[index].item())
-        print("get item")
+        str_folder = self.df_idx.iloc[index].item()
+
+        img = get_data(self.str_data_type, str_folder)
+        img = rearrange(img, 'c s h w -> (c s) h w') # s means sequence
+
+        mask = get_band_mask(self.str_data_type, str_folder)
+        mask = rearrange(mask, 'h w s -> s h w')
+
         return img, mask
 
 
-class ContrailAshDataset_CMP2(torch.utils.data.Dataset):
-    def __init__(self, str_folder):
-        print("init")
-        self.str_data_type = str_folder
-        self.df_idx = pd.DataFrame({'idx':os.listdir(os.path.join("data", str_folder))})
-
-    def __getitem__(self, index):
-        # get_ash_color_images()
-        img = get_ash_color_images(self.df_idx.iloc[index].item(), self.str_data_type) #get_data(self.str_data_type, self.df_idx.iloc[index].item())
-        mask = get_mask_image(self.df_idx.iloc[index].item(), self.str_data_type)
-        print("get item")
-        return img, mask
-
-
-data_train = ContrailAshDataset_CMP1("train")
-img, mask = data_train[0]
-print(f"cmp1 img.shape : {img.shape}, mask.shape : {mask.shape}")
-
-data_train02 = ContrailAshDataset_CMP2("train")
-img2, mask2 = data_train02[0]
-img2 = rearrange(img2, 'h w c s -> (c s) h w')
-mask2 = rearrange(mask2, 'h w s -> s h w')
-print(f"cmp2 img.shape : {img2.shape}, mask.shape : {mask2.shape}")
-
-print(f"equal img : {np.array_equal(img, img2)}, mask : {np.array_equal(mask, mask2)}")
-print(f"img : {img[0][30][30]}, img2 : {img2[0][30][30]}")
+# data_train = ContrailAshDataset("train")
+# img, mask = data_train[0]
+#
+# print(f"img&mask.shape : {img.shape}, {mask.shape}")
--- final_util.py
+++ final_util.py
@@ -8,73 +8,32 @@ import matplotlib.pyplot as plt
 
 from einops import *
 
-
 _T11_BOUNDS = (243, 303)
 _CLOUD_TOP_TDIFF_BOUNDS = (-4, 5)
 _TDIFF_BOUNDS = (-4, 2)
 
+
 def get_band_image(str_data_type, str_folder, num_band_index):
-    print("get_band_iamge")
-    # print(os.path.join("data", str_folder, str(num_band_index)))
     return np.load(os.path.join("data", str_data_type, str_folder, f'band_{num_band_index:02}.npy'))
 
+
 def get_band_mask(str_data_type, str_folder):
-    print("get band mask")
     return np.load(os.path.join("data", str_data_type, str_folder, "human_pixel_masks.npy"))
 
-def normalize_data(data, bounds):
 
+def process_data(data, bounds):
     data = np.clip(data, bounds[0], bounds[1])
-
-    data = rearrange(data, 'h w s -> s h w')
-    # print("normalize data")
-    smooth = 1e-15
-    for i, d in enumerate(data):
-        d = (d - bounds[0]) / (bounds[1] - bounds[0])# + smooth)
-
-        data[i] = d
-
-    return data
+    data = (data - bounds[0]) / (bounds[1] - bounds[0])
+    return rearrange(data, 'h w s -> s h w')
 
 
 def get_data(str_data_type, str_folder):
-    print("get_data")
-    print(f"str_folder : {str_folder}")
     band_11 = get_band_image(str_data_type, str_folder, 11)
     band_14 = get_band_image(str_data_type, str_folder, 14)
     band_15 = get_band_image(str_data_type, str_folder, 15)
 
-    # print(band_14)
-    # filtered_band_14 = normalize_data(band_11)
-    ch1 = normalize_data(band_14, _T11_BOUNDS)#, _T11_BOUNDS[0], _T11_BOUNDS[1]))
-    ch2 = normalize_data((band_14-band_11), _CLOUD_TOP_TDIFF_BOUNDS)
-    ch3 = normalize_data((band_15-band_14), _TDIFF_BOUNDS)
-    # print(ch1)
+    ch1 = process_data((band_15 - band_14), _TDIFF_BOUNDS)
+    ch2 = process_data((band_14 - band_11), _CLOUD_TOP_TDIFF_BOUNDS)
+    ch3 = process_data(band_14, _T11_BOUNDS)
 
-    # ch_rgb = np.concatenate([ch1, ch2, ch3], axis=0)
-    ch_rgb = np.stack([ch1, ch2, ch3])
-
-    ch_rgb = rearrange(ch_rgb, 'c s h w -> (c s) h w')
-
-    print(ch_rgb.shape)
-
-    mask = get_band_mask(str_data_type, str_folder)
-    mask = rearrange(mask, 'h w s -> s h w')
-
-    # print(mask)
-    print(mask.shape)
-
-    # Data Filtering
-    # 14
-    # 14-11
-    # 15-14
-
-    # normalize band 11
-
-
-    # band_test = get_band_image(str_data_type, str_folder, 8)
-    # print(ch1.shape)
-    # print(band_test.shape)
-    # print(band_11)
-
-    return ch_rgb, mask
+    return np.stack([ch1, ch2, ch3])
